666d584d6b6d966aa7f0cc0a2ca2bdf9384c5955
Ritabrata Ghosh <76sonali40@gmail.com>
2025-11-02 15:44:21 +0530

    deploy changes web

--- apps/frontend/config/index.tsx
+++ apps/frontend/config/index.tsx
@@ -1,5 +1,14 @@
-export const HTTP_URL =
-  process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:3001";
-export const WS_URL = "ws://localhost:8000";
-export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+const isProd = process.env.NODE_ENV === "production";
+
+export const HTTP_URL = isProd
+  ? process.env.NEXT_PUBLIC_HTTP_URL ||
+    "https://collabdraw-http-server.onrender.com"
+  : "http://localhost:3001";
+
+export const WS_URL = isProd
+  ? process.env.NEXT_PUBLIC_WS_URL || "https://collabdraw-ws.onrender.com"
+  : "ws://localhost:8000";
+
+export const APP_URL = isProd
+  ? process.env.NEXT_PUBLIC_APP_URL || "https://your-frontend.vercel.app"
+  : "http://localhost:3000";
