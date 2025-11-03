a03b585af8486eeb51acdd0a90313ebbe9f75dcc
Ritabrata Ghosh <76sonali40@gmail.com>
2025-11-04 01:46:57 +0530

    deploy changes 21:4

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -14,7 +14,12 @@
 //   reactStrictMode: true,
 // };
 
-// export default nextConfig;
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
@@ -27,7 +32,23 @@ const nextConfig = {
   },
   transpilePackages: ["@repo/common", "@repo/db", "@repo/kafka"],
   reactStrictMode: true,
-  // Remove the webpack config section
+
+  webpack: (config) => {
+    // Follow symlinks
+    config.resolve.symlinks = true;
+
+    // Explicitly set alias for @repo/common
+    config.resolve.alias["@repo/common"] = path.resolve(
+      __dirname,
+      "../../packages/common/src/types.ts"
+    );
+
+    return config;
+  },
+
+  experimental: {
+    externalDir: true,
+  },
 };
 
 export default nextConfig;
